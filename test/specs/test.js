08021d696c577ebf74f48a5d1c954afaa557db5d
viktor-lut <lvihard@gmail.com>
2019-07-15 19:17:05 +0300

    add cases-14-15-16

--- test/specs/test.js
+++ test/specs/test.js
@@ -117,6 +117,7 @@ describe('Order', function() {
     it('case-10', function() {
         $('#commerce-checkout-form-checkout > div > div > div.item.item-delivery.item-delivery.opened > div.item-content > div > div.b-form-inner > div.radio.by-hand > div.fake-radio.fake-radio-checked').click();
         $('#edit-customer-profile-shipping-string-address').setValue('Россия, Москва, Ленинский проспект, 18');
+        browser.pause(1000);
         $('#edit-customer-profile-shipping-delivery-submit-1').click();
         let sel = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(4) > div.value');
         sel.waitForDisplayed(20000);
@@ -132,6 +133,14 @@ describe('Order', function() {
         sumP = sumP.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
         let sumG = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div.total-bot > div.count > div.value').getText();
         sumG = sumG.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
+
+        const fs = require('fs');
+        const filePath = '/projects/test/test/data/data.json';
+        let data = fs.readFileSync(filePath, "utf-8");
+        data = JSON.parse(data);
+        data.itog = sumG;
+
+        fs.writeFileSync('/projects/test/test/data/data.json', JSON.stringify(data));
         assert.equal((+sumP) + (+sumT), +sumG);
     });
 
@@ -144,22 +153,56 @@ describe('Order', function() {
     });
 
     it('case-13', function() {
-        $('#edit-customer-profile-billing-field-customer-recepient-email-und-0-value').setValue('vihard.test@gmail.com');
+        $('#edit-customer-profile-billing-field-customer-recepient-email-und-0-value').setValue('vihard-test@gmail.com');
         $('#edit-customer-profile-billing-field-user-first-name-und-0-value').setValue('test');
         $('#edit-customer-profile-billing-field-user-last-name-und-0-value').setValue('test');
         $('#edit-customer-profile-billing-field-user-phone-und-0-value').setValue('380601112233');
         $('#edit-customer-profile-billing-field-customer-recepient-profile > div > div > div > div.fake-select-btn').click()
         let drop = $('body > div.fake-select-wrap > div > div > div.fake-select-sub-item.fake-select-sub-item-selected > div');
         drop.click();
-        browser.pause(10000);
         $('#edit-customer-profile-billing-field-user-comment-und-0-value').setValue('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eleifend neque eget purus pretium tincidunt. Donec eros diam, sollicitudin a eleifend sit amet, luctus ac sapien. Donec vitae mollis nisi. Aliquam ut accumsan nisi.');
         $('#edit-customer-profile-billing-agreement').click();
+        browser.pause(1000);
         $('#edit-continue').click();
         let sel = $('#commerce-checkout-form-review > div > div > div.item.item-payment.opened > div.item-content > div > div.b-form-inner');
         sel.waitForDisplayed(20000);
         let box = sel.isDisplayed();
         assert.isTrue(box);
+    });
 
+    it('case-14', function() {
+        $('#edit-commerce-payment-payment-method > div:nth-child(2) > div').click();
+        browser.pause(500);
+        $('#edit-continue').click();
+        let sel = $('body > div.page > div > div.view.view-commerce-cart-form.view-id-commerce_cart_form.view-display-id-cart_order_complete.b-cart-content.view-dom-id-f02016827b0030dd6573d974b0fdd677.jquery-once-1-processed.CartChecker-processed > div > table > tbody');
+        sel.waitForDisplayed(10000);
+        let box = sel.isDisplayed();
+        assert.isTrue(box);
+    });
+
+    it('case-15', function() {
+        const fs = require('fs');
+        const filePath = '/projects/test/test/data/data.json';
+        let data = fs.readFileSync(filePath, "utf-8");
+        data = JSON.parse(data);
+        let info = data.nameP;
+
+        let name = $('/html/body/div[1]/div/div[5]/div/table/tbody/tr/td[1]/div/div[2]').getText();
+
+        assert.equal(info, name);
+    });
+
+    it('case-16', function() {
+        const fs = require('fs');
+        const filePath = '/projects/test/test/data/data.json';
+        let data = fs.readFileSync(filePath, "utf-8");
+        data = JSON.parse(data);
+        let sumItog = data.itog;
+
+        let itogO = $('/html/body/div[1]/div/div[5]/div/table/tbody/tr/td[5]/div[1]').getText();
+        itogO = itogO.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
+
+        assert.equal(sumItog, itogO);
     });
 
 });
