5cf3ded82a29913f0cea5948d9c0f585a0df169e
viktor-lut <lvihard@gmail.com>
2019-07-17 11:46:00 +0300

    update

--- test/specs/test.js
+++ test/specs/test.js
@@ -107,59 +107,61 @@ describe('Order', function() {
     });
 
     it('case-9', function() {
-        $('#edit-delivery-type > div:nth-child(1) > div').click();
+        $('#edit-delivery-type > div:nth-child(2) > div').click();
+        $('#edit-pickup > div:nth-child(3) > div').click();
+        browser.pause(500);
         $('#edit-checkout').click();
-        let sel = $('body > div.page > div > div.b-registration');
+        let sel = $('#commerce-checkout-form-shipping > div > div > div.item.item-info-recipient.opened > div.item-content > div > div.b-form-inner');
         sel.waitForDisplayed(5000);
         let box = sel.isDisplayed();
         assert.isTrue(box);
     });
 
-    it('case-10', function() {
-        $('#commerce-checkout-form-checkout > div > div > div.item.item-delivery.item-delivery.opened > div.item-content > div > div.b-form-inner > div.radio.by-hand > div.fake-radio.fake-radio-checked').click();
-        $('#edit-customer-profile-shipping-string-address').setValue('Россия, Москва, Ленинский проспект, 18');
-        browser.pause(1000);
-        $('#edit-customer-profile-shipping-delivery-submit-1').click();
-        let sel = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(4) > div.value');
-        sel.waitForDisplayed(20000);
-        let box = sel.isDisplayed();
-        assert.isTrue(box);
-    });
+    // it('case-10', function() {
+    //     $('#commerce-checkout-form-checkout > div > div > div.item.item-delivery.item-delivery.opened > div.item-content > div > div.b-form-inner > div.radio.by-hand > div.fake-radio.fake-radio-checked').click();
+    //     $('#edit-customer-profile-shipping-string-address').setValue('Россия, Москва, Ленинский проспект, 18');
+    //     browser.pause(1000);
+    //     $('#edit-customer-profile-shipping-delivery-submit-1').click();
+    //     let sel = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(4) > div.value');
+    //     sel.waitForDisplayed(20000);
+    //     let box = sel.isDisplayed();
+    //     assert.isTrue(box);
+    // });
 
-    it('case-11', function() {
-        let sel = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(3) > div.value')
-        let sumT = sel.getText();
-        sumT = sumT.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
-        let sumP = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(4) > div.value').getText();
-        sumP = sumP.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
-        let sumG = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div.total-bot > div.count > div.value').getText();
-        sumG = sumG.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
+    // it('case-11', function() {
+    //     let sel = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(3) > div.value')
+    //     let sumT = sel.getText();
+    //     sumT = sumT.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
+    //     let sumP = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div:nth-child(4) > div.value').getText();
+    //     sumP = sumP.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
+    //     let sumG = $('body > div.page > div > div.b-registration > div.reg-cart-content > div > div > div.total-bot > div.count > div.value').getText();
+    //     sumG = sumG.replace(/\s+/g, '').slice(0, -4).split(',').join('.');
+    //
+    //     const fs = require('fs');
+    //     const filePath = '/projects/test/test/data/data.json';
+    //     let data = fs.readFileSync(filePath, "utf-8");
+    //     data = JSON.parse(data);
+    //     data.itog = sumG;
+    //
+    //     fs.writeFileSync('/projects/test/test/data/data.json', JSON.stringify(data));
+    //     assert.equal((+sumP) + (+sumT), +sumG);
+    // });
 
-        const fs = require('fs');
-        const filePath = '/projects/test/test/data/data.json';
-        let data = fs.readFileSync(filePath, "utf-8");
-        data = JSON.parse(data);
-        data.itog = sumG;
-
-        fs.writeFileSync('/projects/test/test/data/data.json', JSON.stringify(data));
-        assert.equal((+sumP) + (+sumT), +sumG);
-    });
-
-    it('case-12', function() {
-        $('#edit-continue').click();
-        let sel = $('#commerce-checkout-form-shipping > div > div > div.item.item-info-recipient.opened > div.item-content > div > div.b-form-inner');
-        sel.waitForDisplayed(20000);
-        let box = sel.isDisplayed();
-        assert.isTrue(box);
-    });
+    // it('case-12', function() {
+    //     $('#edit-continue').click();
+    //     let sel = $('#commerce-checkout-form-shipping > div > div > div.item.item-info-recipient.opened > div.item-content > div > div.b-form-inner');
+    //     sel.waitForDisplayed(20000);
+    //     let box = sel.isDisplayed();
+    //     assert.isTrue(box);
+    // });
 
     it('case-13', function() {
         $('#edit-customer-profile-billing-field-customer-recepient-email-und-0-value').setValue('vihard-test@gmail.com');
         $('#edit-customer-profile-billing-field-user-first-name-und-0-value').setValue('test');
         $('#edit-customer-profile-billing-field-user-last-name-und-0-value').setValue('test');
         $('#edit-customer-profile-billing-field-user-phone-und-0-value').setValue('380601112233');
-        $('#edit-customer-profile-billing-field-customer-recepient-profile > div > div').click()
-        let drop = $('body > div.fake-select-sub-main.fake-select-sub-main-opened-bot.fake-select-sub-main-opened > div > div:nth-child(2)');
+        $('#edit-customer-profile-billing-field-customer-recepient-profile > div > div > div#edit-customer-profile-billing-field-customer-recepient-profile > div > div > div').click()
+        let drop = $('body > div:nth-child(19) > div > div > div:nth-child(2) > div');
         browser.pause(500);
         drop.click();
 
